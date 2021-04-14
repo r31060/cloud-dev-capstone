b98b7f58aabcde5ccc3245cc9a0c3dba74b11936
r31060 <jgillies@gmail.com>
2021-04-14 09:33:14 -0700

    changes made to FE for chores

---
+++ client/src/config.ts
@@ -0,0 +1,10 @@
+// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
+const apiId = 'ek2uzcsexf'
+export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/dev`
+
+export const authConfig = {
+  // TODO: Create an Auth0 application and copy values from it into this map
+  domain: 'tk4211.us.auth0.com',            // Auth0 domain
+  clientId: 'zDSRiVkMKfriIvVW0wjy5TST3XgyaYFd',          // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
