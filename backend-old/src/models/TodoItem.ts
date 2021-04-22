9ee75aea5b406246edbdb40e5ad5cdb4ab6bdd26
r31060 <jgillies@gmail.com>
2021-04-22 12:24:49 -0700

    cleanup

--- backend-old/src/models/TodoItem.ts
+++
@@ -1,9 +0,0 @@
-export interface TodoItem {
-  userId: string
-  itemId: string
-  createdAt: string
-  name: string
-  dueDate: string
-  done: boolean
-  attachmentUrl?: string
-}
--- client-old/src/types/Todo.ts
+++
@@ -1,8 +0,0 @@
-export interface Todo {
-  itemId: string
-  createdAt: string
-  name: string
-  dueDate: string
-  done: boolean
-  attachmentUrl?: string
-}
